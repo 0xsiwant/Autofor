94d19c75c391fc89515b48a2c7a41abf38c8a5f9
Bhimsinh Sodha <119826787+bjsodha@users.noreply.github.com>
2023-12-24 10:09:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ API_ID = int(os.environ.get("API_ID", 26930530))
 API_HASH = os.environ.get("API_HASH", "b578cec1f4f5164d952c5a785a399a73")
 
 # Bot token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6510599693:AAFxoVijOtGDcSDVmpU2XSwpRkqRqdXB9i8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6887158447:AAE0eCsWuifUdXLk3cTpPSia31ostfUSY0c")
 
 # Pyrogram session name
-SESSION_NAME = os.environ.get("SESSION_NAME")
+SESSION_NAME = os.environ.get("SESSION_NAME", "ForwadBot")
 
 # MongoDB URI (if you're using MongoDB)
 MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://aio:aio@aio.5z4gxok.mongodb.net/?retryWrites=true&w=majority")
